f0fb81d0f05163cc97b8d5bb4b6e3ff422e42fb2
edna <edna@rr.com>
2018-12-15 17:55:41 +0300

    update login.js

--- UI/javas/login.js
+++ UI/javas/login.js
@@ -1,11 +1,12 @@
 function validate() {
-    var Username = document.getElementById("username").value;
+    var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
     if (username == "admin" && password == "admin") {
         window.location.href = "admin.html";
     }
     else if (username == "user" && password == "user") {
         window.location.href = "profile.html";
     }
-
 }
