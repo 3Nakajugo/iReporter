ad90e384736a343e36268919636e873c05f214ce
edna <edna@rr.com>
2019-02-05 08:11:14 +0300

    add signup.js

---
+++ UI/js/signup.js
@@ -0,0 +1,39 @@
+function signup() {
+    var user = {
+        first_name: document.getElementById("firstname").value,
+        last_name: document.getElementById("lastname").value,
+        telephone: document.getElementById("telephone").value,
+        user_name: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+        isadmin: document.getElementById("isadmin").value
+    }
+    if (document.getElementById('confirmpass').value !== document.getElementById('password').value) {
+        document.getElementById('error').innerHTML = 'password does not match'
+    }
+    else {
+        document.getElementById('error').style.display = 'none'
+        console.log(user)
+        fetch('http://127.0.0.1:5000/api/v2/auth/signup', {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => response.json())
+            .then((response) => {
+                var response_data = response
+                console.log(response_data)
+                if (response_data.status === 400) {
+                    document.getElementById('error').innerHTML = `${response_data.message}`
+                }
+                else if (response_data.status === 201) {
+                    // document.getElementById('error').innerHTML = `${response_data.message}`
+                    window.alert('user was created');
+                    window.location.href = 'login.html';
+                }
+            })
+            .catch(error => console.log(error))
+    }
+}
